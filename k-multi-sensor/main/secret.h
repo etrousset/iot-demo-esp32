0db4cbe53b54b791ef32dc3f18d8ef1e2af7375f
Eric Trousset <etrousset@kaliop.com>
2018-02-16 10:41:58 +0100

    added default empty settings

---
+++ k-multi-sensor/main/secret.h
@@ -0,0 +1,10 @@
+#ifndef __KUZZLE_SECRET_H_
+#define __KUZZLE_SECRET_H_
+#define KUZZLE_IOT_DEVICE_USERNAME "a_kuzzle_user_name"
+#define KUZZLE_IOT_DEVICE_PASSWORD "a_kuzzle_user_admin"
+#define SECRET_DEFAULT_WIFI_SSID ""
+#define SECRET_DEFAULT_WIFI_PASSWORD ""
+#define SECRET_DEFAULT_KUZZLE_HOSTNAME "default_kuzzle_hostname"
+#define SECRET_DEFAULT_KUZZLE_MQTT_PORT 1883
+
+#endif // __KUZZLE_SECRET_H_
